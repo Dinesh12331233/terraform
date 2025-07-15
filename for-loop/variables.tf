bd0146c1f8e0b3986c71601c1efbced166327fbd
Dinesh Kancherla <dineshkancherla8@gmail.com>
2025-07-15 12:43:05 +0530

    terraform

---
+++ for-loop/variables.tf
@@ -0,0 +1,64 @@
+variable "ami_id" {   # Variable
+    type    = string  # Optional
+    default = "ami-09c813fb71547fc4f" # Value # Mandatory
+    description = "AMI ID of joindevops RHEL9" # Optional
+}
+
+variable "instance_type" {
+    default = "t3.micro"     # here type is string
+}
+
+variable "ec2_tags" { 
+    type    = map(string) # map of string means map lopala strings unnayi
+    default = {         # here type is Map 
+        Name = "roboshop" 
+        Purpose = "variables-demo" 
+    }
+}
+
+variable "sg_name" {
+    default = "vars-file-allow-all" 
+}
+
+variable "sg_description" {
+    default = "allowing all from internet" 
+} 
+
+variable "from_port" {
+    default = 0 
+} 
+
+variable "to_port" { 
+    type = number # This is optional
+    default = 0 
+} 
+
+variable "cidr_blocks" {
+    type = list(string)  
+    default = ["0.0.0.0/0"] 
+} 
+
+variable "sg_tags" {
+    type = map(string) 
+    default = {
+      Name = "allow-all" 
+    }
+}
+
+variable "instances" {
+    default = {
+        mongodb = "t3.micro" # each keyword is assigned for every iteration. so you will get each.key and each.value 
+        redis   = "t3.micro" 
+        mysql   = "t3.small" 
+        rabbitmq= "t3.micro" 
+    }
+} 
+
+variable "zone_id" {
+    default = "Z0369172UBR468QD4YPX"
+} 
+
+variable "domain_name" {
+    default = "deepthi.tech" 
+}
+
